e24e4ca2f4310f8b282c5946fd952b0b32c129aa
Eugenia <pyssiki@yandex.ru>
2022-01-13 17:59:30 +0500

    calculate

---
+++ calculate 2/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  calculate 2
+//
+//  Created by Евгения Аникина on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var numberFromScreen:Double = 0;
+    var firstNum:Double = 0;
+    var operation:Int = 0;
+    var mathSight:Bool = false;
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if mathSight == true {
+            result.text = String(sender.tag)
+            mathSight = false
+        }
+        else {
+            result.text = result.text! + String(sender.tag)
+        }
+        numberFromScreen = Double(result.text!)!
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if result.text != "" && sender.tag != 10 && sender.tag != 15 {
+            
+            firstNum = Double(result.text!)!
+                        
+            if sender.tag == 11 {// деление
+                result.text = "/";
+            }
+            else if sender.tag == 12 { //умножение
+                result.text = "x";
+            }
+            else if sender.tag == 13 { //вычитание
+                result.text = "-";
+            }
+            else if sender.tag == 14 { //добавление
+                result.text = "+";
+            }
+            
+            operation = sender.tag
+            
+            mathSight = true;
+        }
+        else if sender.tag == 15 { //посчитать все
+            if operation == 11 {
+                result.text = String(firstNum / numberFromScreen)
+            }
+            else if operation == 12 {
+                result.text = String(firstNum * numberFromScreen)
+        }
+            else if operation == 13 {
+                result.text = String(firstNum - numberFromScreen)
+           }
+            else if operation == 14 {
+                result.text = String(firstNum + numberFromScreen)
+        }
+      }
+        else if sender.tag == 10 {
+            result.text = ""
+            firstNum = 0
+            numberFromScreen = 0
+            operation = 0
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ calculate 2UITests/calculate_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  calculate_2UITestsLaunchTests.swift
+//  calculate 2UITests
+//
+//  Created by Евгения Аникина on 12.01.2022.
+//
+
+import XCTest
+
+class calculate_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
